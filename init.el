e46afdaddf4512f8d6a1ab79ae32eb6b1b686186
salim <salim.belkacemm@protonmail.com>
2026-02-02 13:53:33 +0100

    all in one file idc

--- init.el
+++ init.el
@@ -1,12 +1,51 @@
-(load "~/.emacs.d/packages.el") (setq custom-file "~/.emacs.d/custom.el")
+(setq custom-file "~/.emacs.d/custom.el")
 
+;;; PACKAGES -----------------------
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(setq package-selected-packages
+      '(evil
+	evil-leader
+	evil-collection
+
+	magit
+
+	kanagawa-themes
+	spacious-padding
+	dashboard
+
+	which-key
+
+	tree-sitter-langs
+
+	treemacs
+
+	lsp-mode
+	lsp-treemacs
+	lsp-ui
+
+	company
+
+	yasnippet
+	yasnippet-snippets
+
+	flycheck
+	))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
 
 ;;; LOOKS AND FEELS ----------------
-
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 (global-display-line-numbers-mode)
+(electric-pair-mode)
 
 (setq-default display-line-numbers-type 'relative)
 (setq create-lockfiles nil)
@@ -15,27 +54,33 @@
 (setq make-backup-files nil)
 (setq scroll-margin 10)
 
+(set-face-attribute 'fill-column-indicator nil
+                      :foreground "#717C7C"
+                      :background "transparent")
+
 (spacious-padding-mode 1)
 
 (setq spacious-padding-widths
-   '(:internal-border-width 20 :header-line-width 4 :mode-line-width 6
-			    :custom-button-width 3 :tab-width 4
-			    :right-divider-width 30 :scroll-bar-width
-			    8 :fringe-width 8))
+      '(:internal-border-width 20
+	:header-line-width 4
+	:mode-line-width 6
+	:custom-button-width 3 
+	:tab-width 4
+	:right-divider-width 30
+	:scroll-bar-width 8
+	:fringe-width 8))
 
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font-10")
 
-;;; PACKAGES? ---------------------
+;;; PACKAGES CONFIG ---------------------
 (setq evil-want-keybinding nil)
 (setq evil-undo-system 'undo-redo)
+(setq initial-buffer-choice 'dashboard-open)
 
-(require 'kanagawa-themes)
 (require 'evil)
 (require 'evil-leader)
 (require 'evil-collection)
 (require 'dashboard)
-
-(setq initial-buffer-choice 'dashboard-open)
 
 ;; theme
 (load-theme 'kanagawa-dragon t)
@@ -50,7 +95,7 @@
 (evil-leader/set-leader "SPC")
 
 (evil-leader/set-key
-  "e" 'dired-jump
+  "e" 'treemacs
 
   "gg" 'magit-status
   "gc" 'magit-commit-create
@@ -59,15 +104,45 @@
   "gP" 'magit-pull
 
   "cc" 'compile
+
+  "t" 'tetris
   )
 
 (evil-mode 1)
+(which-key-mode)
 
+(use-package treemacs
+  :ensure t
+  :defer t
+  :config
+  (progn (setq treemacs-position 'right)))
 
 ;;; SNIPPETS, HIGHLIGHTS AND LSP ---------------
-(require 'yasnippet)
+(setq lsp-warn-no-matched-clients nil)
 
-(add-hook 'prog-mode-hook 'eglot-ensure)
-(add-hook 'prog-mode-hook #'tree-sitter-mode)
+(setq company-backends
+      '((company-capf company-yasnippet)
+        company-files
+        company-dabbrev))
+(setq company-minimum-prefix-length 1)
+(setq company-idle-delay 0.1)
+(global-company-mode)
 
-(yas-global-mode 1)
+(setq major-mode-remap-alist
+      '((c-mode           . c-ts-mode)
+        (c++-mode         . c++-ts-mode)
+        (go-mode          . go-ts-mode)
+        (java-mode        . java-ts-mode)
+        (python-mode      . python-ts-mode)
+        (js-mode          . js-ts-mode)
+        (typescript-mode  . typescript-ts-mode)
+        (json-mode        . json-ts-mode)
+        (css-mode         . css-ts-mode)
+        ))
+(add-hook 'prog-mode-hook 'tree-sitter-hl-mode)
+
+(add-hook 'prog-mode-hook #'lsp)
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (yas-global-mode))
+
